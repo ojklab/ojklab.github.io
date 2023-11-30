e2091266d1571f2cdda50fadb4bdaf2111d693aa
OJK <26958104+ojklab@users.noreply.github.com>
2023-11-30 19:21:28 +0900

    更新

---
+++ lab/2023/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  //ページ内スクロール
+  var navHeight = $("header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position }, 300, "swing");
+    return false;
+  });
+});
